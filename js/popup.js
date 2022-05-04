7e54298a548b3c139c3a9c3e61cd08da101d2584
Nikita <nik.andreev.99@yandex.ru>
2022-05-04 22:12:42 +0300

    add id

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 	class Popup {
 		#content
+		#id
 		#popupEl
 		#popupClasses
 		#bodyClasses
@@ -10,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 		constructor(config) {
 			this.#content = config.content || ''
+			this.#id = config.id || ''
 			this.#popupClasses = config.popupClasses || []
 			this.#bodyClasses = config.bodyClasses || []
 			this.#contentClasses = config.contentClasses || []
@@ -41,6 +43,15 @@ document.addEventListener('DOMContentLoaded', function(){
 
 		destroy () {
 			this.#popupEl.remove()
+
+			this.#content = null
+			this.#popupEl = null
+			this.#id = null
+			this.#popupClasses = null
+			this.#bodyClasses = null
+			this.#contentClasses = null
+			this.#closeClasses = null
+
 			document.removeEventListener('keydown', this.#escKeyDownHandler)
 		}
 
@@ -85,7 +96,7 @@ document.addEventListener('DOMContentLoaded', function(){
 		}
 
 		#getPopupHtml() {
-			return `<div class="popup ${this.#popupClasses.join(' ')}">
+			return `<div class="popup ${this.#popupClasses.join(' ')}" ${this.#id ? `id="${this.#id}"` : ''}>
 								<div class="popup__body ${this.#bodyClasses.join(' ')}">
 									<div class="popup__content ${this.#contentClasses.join(' ')}" data-popup-content>
 										${this.#content}
@@ -102,7 +113,7 @@ document.addEventListener('DOMContentLoaded', function(){
 								<div class="popup__text">
 									Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, aspernatur deleniti. Soluta nostrum id libero nobis possimus eveniet magni neque incidunt sunt pariatur, quisquam iste aliquid repudiandae sapiente delectus amet omnis, accusamus exercitationem. Illum voluptatem magni excepturi mollitia incidunt, enim doloremque accusamus voluptatibus distinctio facere quas corporis dolorum ipsam quae, minus tempore voluptas saepe reiciendis. Autem at voluptatem esse. Iusto deserunt rerum deleniti, quisquam dignissimos officiis debitis obcaecati repellendus quae, sit cum laboriosam maxime atque delectus saepe eveniet, a itaque eligendi quos. Asperiores, dolorem inventore doloremque maiores soluta optio iusto esse perspiciatis, provident numquam mollitia quae ratione maxime dolore quos.
 								</div>
-								<button data-popup-close>Close</button>`			
+								<button data-popup-close>Close</button>`	
 		}).show()
 	})
 	
